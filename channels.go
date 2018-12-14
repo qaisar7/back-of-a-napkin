7deccef95dc0b621ad68616ffe49b9be17dc9944
Qaisar Khan <qaisar.sardar@gmail.com>
2018-12-15 10:17:52 +1100

    Make threads process jobs from a job line concurrently

--- channels.go
+++ channels.go
@@ -11,48 +11,65 @@ import (
 	"time"
 )
 
-func main() {
-	fmt.Println("Hello, playground")
-	c := make(chan int, 1)
-	threads := []chan int{}
-	shutdown := make(chan struct{})
-	for i := 0; i < 6; i++ {
-		t := make(chan int)
-		threads = append(threads, t)
-		go func(i int, c chan int, t chan int) {
-			fmt.Printf("Thread %d sleeping now\n", i)
-			tm := time.Now()
-			d := time.Duration(rand.Int63n(6000))
-			fmt.Println("duration ", time.Millisecond*d)
-			select {
-			case <-time.After(time.Millisecond * d):
-				fmt.Printf("Thread %d waking after %s \n", i, time.Now().Sub(tm))
-				c <- i
-			case <-shutdown:
-				fmt.Printf("Thread %d shutting down\n", i)
-				close(c)
-			}
-
-			t <- i
-			close(t)
-		}(i, c, t)
+// generateInput generates a slice of size 'n' each having a random time duration number between 0-6000.
+func generateInput(n int) []time.Duration {
+	ar := []time.Duration{}
+	for i := 0; i < n; i++ {
+		d := time.Duration(rand.Int63n(6000))
+		ar = append(ar, d)
 	}
+	return ar
+}
 
+type threadId struct {
+	id   int
+	jobs int
+}
+
+func main() {
+	startTime := time.Now()
+	waits := generateInput(10)
+	fmt.Println("channnnnnnels", waits)
+	totalThreads := 3
+	jobC := make(chan time.Duration, totalThreads)
+	//shutdown := make(chan struct{})
 	go func() {
-		for _, t := range threads {
-			for i := range t {
-				fmt.Printf("thread %d closed\n", i)
-			}
+		for _, w := range waits {
+			fmt.Printf("sending wait %s on job channel\n", time.Millisecond*w)
+			jobC <- w
 		}
-		close(c)
+		fmt.Println("closing job channel")
+		close(jobC)
+		//close(shutdown)
 	}()
 
-	for i := range c {
-		fmt.Printf("Thread %d done\n", i)
-		if i == 3 {
-			close(shutdown)
-			fmt.Println(" got result, closing")
-			break
+	threads := []chan threadId{}
+
+	for i := 0; i < totalThreads; i++ {
+		t := make(chan threadId)
+		threads = append(threads, t)
+		go func(i int, c chan time.Duration, t chan threadId) {
+			jobs := 0
+			for d := range c {
+				fmt.Printf("Thread %d sleeping now for %s \n", i, time.Millisecond*d)
+				tm := time.Now()
+				time.Sleep(time.Millisecond * d)
+				fmt.Printf("Thread %d waking after %s \n", i, time.Now().Sub(tm))
+				jobs++
+			}
+			t <- threadId{i, jobs}
+			close(t)
+		}(i, jobC, t)
+	}
+
+	for _, t := range threads {
+		for th := range t {
+			fmt.Printf("thread %d closed with jobs %d\n", th.id, th.jobs)
 		}
 	}
+	var totalWaits time.Duration
+	for _, w := range waits {
+		totalWaits += w
+	}
+	fmt.Printf("total program time %s for total waits %s\n", time.Now().Sub(startTime), time.Millisecond*totalWaits)
 }
